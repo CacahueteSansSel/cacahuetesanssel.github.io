23fe556cf65b11fc41abd387125322cf0d26c3ed
Cacahuète <24205659+CacahueteSansSel@users.noreply.github.com>
2023-01-24 22:11:18 +0100

    Initial webpage design

---
+++ main.js
@@ -0,0 +1,11 @@
+function seeShelter() {
+    window.location = 'https://cacahuete-dev.itch.io/shelter';
+}
+
+function seeNightdrive() {
+    window.location = 'https://play.google.com/store/apps/details?id=dev.cacahuete.nightdrive';
+}
+
+function seeIL() {
+    window.location = 'https://cacahuete-dev.itch.io/infinite-library';
+}
